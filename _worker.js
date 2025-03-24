50de4603a21073632a92c28ac8a41925ab6f5a43
DaChunHe <130650729+DaChunHe@users.noreply.github.com>
2025-03-24 11:31:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -96,7 +96,7 @@ export default {
 			} else {
 				if (new RegExp('/proxyip=', 'i').test(url.pathname)) proxyIP = url.pathname.split("=")[1];
 				else if (new RegExp('/proxyip.', 'i').test(url.pathname)) proxyIP = url.pathname.split("/proxyip.")[1];
-				else if (!proxyIP || proxyIP == '') proxyIP = 'proxyip.fxxk.dedyn.io';
+				else if (!proxyIP || proxyIP == '') proxyIP = 'proxyip.cmliussss.net ';
 				return await vlessOverWSHandler(request);
 			}
 		} catch (err) {
